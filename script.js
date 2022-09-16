6ce39b1f769e9165ea5d13ce72dd0f108cae3008
Dmitri <dmitri.antipov1@gmail.com>
2022-09-16 10:57:21 +0300

    15.09.2022

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const countdown = () => {
     // const countDate = new Date('April 14, 2022 13:55:00').getTime()
-    const countDate = new Date('August 30, 2022 08:47:00').getTime()
+    const countDate = new Date('September 15, 2022 08:47:00').getTime()
     const now = new Date().getTime()
     const gap = now - countDate
 
